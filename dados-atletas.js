e2a735d68435efd6ed9bbe2f6dfc8abe7be164a3
D0mAl3x <asilvapi2012@gmail.com>
2024-07-23 23:44:15 -0300

    Comentários adicionais incluídos
    Inseri comentários adicionais que considerei pertinentes para explicitar melhor o algoritmo.

--- dados-atletas.js
+++ dados-atletas.js
@@ -31,6 +31,14 @@ class Atleta{
 
     // MÉTODOS PARA O CÁLCULO E OBTENÇÃO DA CATEGORIA E DO IMC DO(A) ATLETA:
 
+    // Regras para calcular a categoria:
+
+    // Infantil: 9 a 11 anos
+    // Juvenil: 12 e 13 anos
+    // Intermediário: 14 e 15 anos
+    // Adulto: 16 a 30 anos
+    // Sem categoria: demais idades
+
     calculaCategoria(idade){
         let categoria;
         if((idade<9)||(idade>30)){
@@ -53,51 +61,54 @@ class Atleta{
         return categoria;
     }
 
+    // Para calcular o IMC, usamos a fórmula: imc = peso / (altura x altura):
+
     calculaIMC(peso, altura){
         return peso/(altura*altura);
     }
 
     obtemCategoria(){
-        return this.calculaCategoria(this.obtemIdadeAtleta());
+        return this.calculaCategoria(this.obtemIdadeAtleta()); // fornecemos a idade armazenada no atributo idade.
     }
 
     obtemIMC(){
         return this.calculaIMC(
-            this.obtemPesoAtleta(),this.obtemAlturaAtleta()
-        );
+            this.obtemPesoAtleta(),this.obtemAlturaAtleta() // fornecemos o peso e a altura armazenados nos respectivos
+        );                                                  // atributos do objeto
     }
 
     // MÉTODOS PARA O CÁLCULO E A OBTENÇÃO DA MÉDIA VÁLIDA DO ATLETA:
 
     ordenaNotas(notas) {
-        let notasOrdenadas = notas.sort(function (a, b) {
+        let notasOrdenadas = notas.sort(function (a, b) { // usamos o método .sort() para ordenar as notas
                     return a - b;
                 });
         return notasOrdenadas;
     }
 
-    extraiNotasValidas(notasOrdenadas) {
-        let notasValidas = notasOrdenadas.slice(1,this.obtemNotasAtleta.length-1);
-        return notasValidas;
-    }
-
+    extraiNotasValidas(notasOrdenadas) {                                           // utilizamos o método .slice()
+        let notasValidas = notasOrdenadas.slice(1,this.obtemNotasAtleta.length-1); // para obter um sub-array contendo
+        return notasValidas;                                                       // apenas as notas válidas. No seu
+    }                                                                              // segundo argumento consultamos o
+                                                                                // tamanho do array de notas (atributo
+                                                                                // do objeto) para evitar o hardcoding.
     somarNotasValidas(notasValidas) {
         let total = 0;
-        total = notasValidas.reduce(function (soma,contador) {
-            return soma + contador;
+        total = notasValidas.reduce(function (soma,contador) { // usamos o método .reduce() para calcular o somatório
+            return soma + contador;                            // das notas válidas.
         });
         return total;
     }
 
-    calculaMediaValida(notasValidas, somatorioNotas) {
-        return somatorioNotas / notasValidas.length;
+    calculaMediaValida(notasValidas, somatorioNotas) { // calculamos a média dividindo o somatório das notas válidas
+        return somatorioNotas / notasValidas.length;   // pelo tamanho do array que as armazena.
     }
 
     obtemMediaValida() {
         let notasValidas = this.extraiNotasValidas(this.ordenaNotas(this.obtemNotasAtleta()));
         let somatorio = this.somarNotasValidas(notasValidas);
-        return this.calculaMediaValida(notasValidas, somatorio);
-    }
+        return this.calculaMediaValida(notasValidas, somatorio); // aqui criamos as variáveis locais apenas para
+    }                                                            // facilitar a chamada ao método calculaMediaValida().
 }
 
 
